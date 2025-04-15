c38b86b5c625c9dcfdae5031dd67dd295f4ced1d
yume@PC <34027873+yuandahanif@users.noreply.github.com>
2025-04-15 19:24:36 +0700

    add tailwind and shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { render } from 'preact'
 import './index.css'
-import { App } from './app.tsx'
+import { App } from './pages/index'
 
 render(<App />, document.getElementById('app')!)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import path from "path";
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [preact(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
